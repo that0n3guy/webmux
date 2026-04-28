786d571a7f567b74dc99b65ecdb3c579e50fed06
Peter Olson <[email protected]>
2026-04-28 15:16:39 -0500

    fix(contract): add 422 to commonErrorResponses for git op failures

--- packages/api-contract/src/contract.ts
+++ packages/api-contract/src/contract.ts
@@ -86,6 +86,7 @@ const commonErrorResponses = {
   400: ErrorResponseSchema,
   404: ErrorResponseSchema,
   409: ErrorResponseSchema,
+  422: ErrorResponseSchema,
   500: ErrorResponseSchema,
   502: ErrorResponseSchema,
   503: ErrorResponseSchema,
